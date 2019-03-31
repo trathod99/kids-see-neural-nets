14a012f9b6564fbf1970f3347aff35da4b9d255a
tyler <tyler.visca.barca@gmail.com>
2019-03-31 16:27:41 -0700

    working lyric API

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+// {"token_type":"bearer","access_token":"AAAAAAAAAAAAAAAAAAAAAIb19gAAAAAAY1ozuEmN%2BPqdpA5rXm%2FwG2Qf4Cg%3Dv2aixiExWlsKL3wE4SF9m52um6ovUXVWdQe7aZYDwFqPFmiB9I"}⏎
 const brain = require("brain.js")
 const network = new brain.recurrent.LSTM();
