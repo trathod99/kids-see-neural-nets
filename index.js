f918ee60b107253542115d5db60a8638a48ec7cf
tyler <tyler.visca.barca@gmail.com>
2019-04-05 17:38:10 -0700

    working kanye/trump tweet prediction - SLOW though even with low iteration count

--- index.js
+++ index.js
@@ -3,72 +3,75 @@ const brain = require("brain.js");
 const request = require("request");
 const network = new brain.recurrent.LSTM();
 
-const options = {
-    url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=kanyewest&count=100',
+const kanyeOptions = {
+    url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=kanyewest&count=20',
+    headers: {
+      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAIb19gAAAAAAY1ozuEmN%2BPqdpA5rXm%2FwG2Qf4Cg%3Dv2aixiExWlsKL3wE4SF9m52um6ovUXVWdQe7aZYDwFqPFmiB9I'
+    }
+};
+
+const trumpOptions = {
+    url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realDonaldTrump&count=20',
     headers: {
       'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAIb19gAAAAAAY1ozuEmN%2BPqdpA5rXm%2FwG2Qf4Cg%3Dv2aixiExWlsKL3wE4SF9m52um6ovUXVWdQe7aZYDwFqPFmiB9I'
     }
 };
 
 var info = [];
-var kanye_tweets = new Array();
+tweet_json_data = []
+
+function Tweet(text, account) {
+    this.text = text;
+    this.account = account;
+}
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-      info = JSON.parse(body);
-      info.forEach(tweet => {
-          kanye_tweets.push(tweet.text);
-      });
-      console.log(kanye_tweets);
+        info = JSON.parse(body);
+        info.forEach(tweet => {
+            let t = new Tweet(tweet.text, tweet.user.name);
+            tweet_json_data.push(t);
+        });
+        console.log("---------------")
+        console.dir(tweet_json_data);
     }
 }
 
-/*
-
-
-
-
-IT MIGHT BE A TIMEOUT ISSUE - ADD A TIMEOUT TO THE JSON REQUEST OR SOMETHING
-
-
-
-
-*/
-
-request(options, callback);
-
-const kanye_tweet = {
-    text: "",
-    account: "Kanye"
-}
-
-kanye_json_data = []
-
-// Turns array of tweets into an array of JSON objects - DOESN'T WORK
-function textToJSON(text) {
-    /* var isArr = kanye_tweets instanceof Array;
-    console.log(isArr); */
-    console.log(kanye_tweets); 
-    kanye_tweets.forEach(tweet_text => {
-        t = new kanye_tweet;
-        t.text = tweet_text;
-        kanye_json_data.push(t);
-    });
-}
-
-textToJSON(kanye_tweets);
-
-console.log(kanye_json_data);
-
-/* const trainingData = data.map(item => ({
-    input: item.text,
-    output: item.category
-}));
-
-network.train(trainingData, {
-    iterations: 100
+makeRequests( function() {
+    setTimeout( function() {
+        runNeuralNet(populateTrainingData(tweet_json_data));
+    }, 10000);
 });
 
-const output = network.run("Hard drive power");
 
-console.log(`Category: ${output}`); */
+// Turns array of tweets into an array of JSON objects - DOESN'T WORK
+/* function textToJSON(text) {
+    tweets.forEach(tweet_text => {
+        let t = new Tweet(tweet_text);
+        tweet_json_data.push(t);
+    });
+} */
+
+function makeRequests(net) {
+    request(kanyeOptions, callback);
+    request(trumpOptions, callback);
+    net();
+}
+
+function populateTrainingData(data) {
+    const trainingData = data.map(item => ({
+        input: item.text,
+        output: item.account
+    }));
+    console.log("-----------------")
+    console.log(trainingData);
+    return trainingData;
+}
+
+function runNeuralNet(trainingData) {
+    network.train(trainingData, {
+        iterations: 100
+    });
+    const output = network.run("artist kanye drake love");
+    console.log(`Account: ${output}`);
+}
